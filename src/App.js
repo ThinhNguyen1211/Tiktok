6e01ac5b48ecbe767110e944a0be3e2adb937991
Thịnh Nguyễn <nguyentuanthinh1211@gmail.com>
2023-09-09 19:50:12 +0700

    new changes 2

--- src/App.js
+++ src/App.js
@@ -5,8 +5,6 @@ const App = () => {
     <div style={{padding: '32px'}}>
       <h1>Hello ReactJS!</h1>
       <p>Test Commit 1</p>
-      <p>Test Commit 2</p>
-      <p>Test Commit 3</p>
     </div>
   )
 }
