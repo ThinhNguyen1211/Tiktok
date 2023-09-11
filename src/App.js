778c7f8b75bdb9f8d2094d3f246de0a1d745feaa
Thịnh Nguyễn <nguyentuanthinh1211@gmail.com>
2023-09-12 01:22:28 +0700

    Cấu hình Router / Layout
    cấu hình customize-cra, babel-plugin-module-resolver, prettier, Sass

--- src/App.js
+++ src/App.js
@@ -1,12 +1,51 @@
-
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { publicRoutes } from '~/routes';
+import { DefaultLayout } from '~/components/layout';
+import { Fragment } from 'react';
 
 const App = () => {
-  return (
-    <div style={{padding: '32px'}}>
-      <h1>Hello ReactJS!</h1>
-      <p>Test Commit 1</p>
-    </div>
-  )
-}
+    return (
+        <Router>
+            <Link to="/" style={{ padding: '10px' }}>
+                Home
+            </Link>
+            <Link to="/following" style={{ padding: '10px' }}>
+                Following
+            </Link>
+            <Link to="/profile" style={{ padding: '10px' }}>
+                Profile
+            </Link>
+            <Link to="/upload" style={{ padding: '10px' }}>
+                Upload
+            </Link>
+            <div style={{ padding: '32px' }}>
+                <Routes>
+                    {publicRoutes.map((route, i) => {
+                        let Layout = DefaultLayout;
+                        const Page = route.component;
 
-export default App
+                        if (route.layout) {
+                            Layout = route.layout;
+                        } else if (route.layout === null) {
+                            Layout = Fragment;
+                        }
+
+                        return (
+                            <Route
+                                key={i}
+                                path={route.path}
+                                element={
+                                    <Layout>
+                                        <Page />
+                                    </Layout>
+                                }
+                            />
+                        );
+                    })}
+                </Routes>
+            </div>
+        </Router>
+    );
+};
+
+export default App;
---
+++ src/components/layout/DefaultLayout/index.js
@@ -0,0 +1,16 @@
+import Header from '~/components/layout/components/Header';
+import Sidebar from '~/components/layout/components/Sidebar';
+
+function DefaultLayout({ children }) {
+    return (
+        <div>
+            <Header />
+            <div className="container">
+                <Sidebar />
+                <div className="content">{children}</div>
+            </div>
+        </div>
+    );
+}
+
+export default DefaultLayout;
