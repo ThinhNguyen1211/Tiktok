e226fa62f73721ea230b754c2a03e23ae40650d2
Thịnh Nguyễn <nguyentuanthinh1211@gmail.com>
2023-09-16 04:32:25 +0700

    copy các icon, logo

---
+++ src/assets/images/index.js
@@ -0,0 +1,24 @@
+const Image = {
+    tiktokLogo: require('~/assets/images/tiktokLogo.svg').default,
+    searchIcon: require('~/assets/images/Icons/searchIcon.svg').default,
+    plusIcon: require('~/assets/images/Icons/plusIcon.svg').default,
+    deviceChangedIcon: require('~/assets/images/Icons/deviceChangedIcon.svg').default,
+    menuIcon: require('~/assets/images/Icons/menuIcon.svg').default,
+    creativeBulbIcon: require('~/assets/images/Icons/creativeBulbIcon.svg').default,
+    languagesSelectIcon: require('~/assets/images/Icons/languagesSelectIcon.svg').default,
+    helpIcon: require('~/assets/images/Icons/helpIcon.svg').default,
+    keyboardIcon: require('~/assets/images/Icons/keyboardIcon.svg').default,
+    nightModeIcon: require('~/assets/images/Icons/nightModeIcon.svg').default,
+    homeIcon: require('~/assets/images/Icons/homeIcon.svg').default,
+    followIcon: require('~/assets/images/Icons/followIcon.svg').default,
+    exploreIcon: require('~/assets/images/Icons/exploreIcon.svg').default,
+    liveIcon: require('~/assets/images/Icons/liveIcon.svg').default,
+    loveIcon: require('~/assets/images/Icons/loveIcon.svg').default,
+    commentsIcon: require('~/assets/images/Icons/commentsIcon.svg').default,
+    saveIcon: require('~/assets/images/Icons/saveIcon.svg').default,
+    shareIcon: require('~/assets/images/Icons/shareIcon.svg').default,
+    clearIcon: require('~/assets/images/Icons/clearIcon.svg').default,
+    loadingIcon: require('~/assets/images/Icons/loadingIcon.svg').default,
+};
+
+export default Image;
--- src/components/Layouts/components/Header/index.js
+++ src/components/Layouts/components/Header/index.js
@@ -1,12 +1,36 @@
 import classNames from 'classnames/bind';
 import styles from './Header.module.scss';
+import images from '~/assets/images';
 
 const cx = classNames.bind(styles);
 
 function Header() {
     return (
         <header className={cx('wrapper')}>
-            <div className={cx('inner')}></div>
+            <div className={cx('inner')}>
+                <div className={cx('logo')}>
+                    <img src={images.tiktokLogo} alt="Tiktok logo" />
+                </div>
+                <div className={cx('search')}>
+                    <input placeholder="Search accounts and videos" spellCheck={false} />
+                    <span className="spanSpliter"></span>
+                    <button className={cx('clear')}>
+                        <img src={images.clearIcon} alt="clear icon" className={cx('clear-icon')} />
+                    </button>
+                    <button className={cx('loading')}>
+                        <img src={images.loadingIcon} alt="clear icon" className={cx('loading-icon')} />
+                    </button>
+                    <button className={cx('search-btn')}>
+                        <img src={images.searchIcon} alt="Search logo" />
+                    </button>
+                </div>
+                <div className={cx('settings')}>
+                    <a>
+                        <div></div>
+                    </a>
+                    <button>Đăng nhập</button>
+                </div>
+            </div>
         </header>
     );
 }
